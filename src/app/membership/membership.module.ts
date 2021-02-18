35919afa4d906f908544799c30113f88eff58059
unknown <edwinjosekdk@gmail.com>
2021-02-18 11:44:20 +0530

    u[p

--- src/app/membership/membership.module.ts
+++ src/app/membership/membership.module.ts
@@ -6,10 +6,11 @@ import { MembershipComponent } from './membership.component';
 import { MaterialModule } from '../components/material/material.module';
 import { CommonsModule } from '../components/commons/commons.module';
 import { InfoComponent } from './info/info.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 
 @NgModule({
-  declarations: [MembershipComponent, InfoComponent],
+  declarations: [MembershipComponent, InfoComponent, RoomsComponent],
   imports: [
     CommonModule,
     MembershipRoutingModule,
