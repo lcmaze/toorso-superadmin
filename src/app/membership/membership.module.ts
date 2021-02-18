74fe70bbd5acb55f071a411401a56a05d0653a55
unknown <edwinjosekdk@gmail.com>
2021-02-18 17:43:48 +0530

    update

--- src/app/membership/membership.module.ts
+++ src/app/membership/membership.module.ts
@@ -7,10 +7,11 @@ import { MaterialModule } from '../components/material/material.module';
 import { CommonsModule } from '../components/commons/commons.module';
 import { InfoComponent } from './info/info.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { FeaturesComponent } from './features/features.component';
 
 
 @NgModule({
-  declarations: [MembershipComponent, InfoComponent, RoomsComponent],
+  declarations: [MembershipComponent, InfoComponent, RoomsComponent, FeaturesComponent],
   imports: [
     CommonModule,
     MembershipRoutingModule,
